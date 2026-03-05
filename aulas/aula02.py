f4a9ad1aee91d38ad5fc994e724fdedafc97bf8f
leonardocandido-cmyk <leonardo.candido@escola.pr.gov.br>
2026-03-05 12:47:06 +0000

    Entendendo tipos de variáveis em python

---
+++ aulas/aula02.py
@@ -0,0 +1,9 @@
+# Dados numéricos
+umidade_do_ar=80
+temperatura=26
+velocidade_do_vento=20
+
+# Dados Categoricos
+cidades = ['Verê', 'Dois Vizinhos', 'Itapejara', 'Fco. Beltrão'
+           , 'São João']
+
