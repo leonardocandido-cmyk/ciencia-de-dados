b7c7400fb9700cb97cd2d30298db0721906539a9
leonardocandido-cmyk <leonardo.candido@escola.pr.gov.br>
2026-05-28 12:46:41 +0000

    Aula 06 - Análise quantiativa, usando histograma

--- aulas/aula06.py
+++ aulas/aula06.py
@@ -22,3 +22,26 @@ plt.ylabel("Frequência")
 
 plt.show()
 plt.savefig("aula06-qualitativo")
+
+plt.clf()
+
+# Análise quantitativa
+notas = [
+    5, 6, 7, 8, 7,
+    6, 5, 9, 10, 8,
+    7, 6, 5, 8, 9
+]
+
+serie = pd.Series(notas)
+frequencia = serie.value_counts()
+
+print(frequencia)
+
+serie.plot(kind="hist")
+
+plt.title("Distribuição das Notas")
+plt.xlabel("Notas")
+plt.ylabel("Frequência")
+
+plt.show()
+plt.savefig("aula06-quantitativo")
